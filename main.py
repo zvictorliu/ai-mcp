8b874c815ad680b22e35d3788a74ca937f51033a
zvictorliu <zvictorliu@outlook.com>
2025-12-24 23:04:19 +0800

    feat: 基于 fastmcp 注册 mcp 服务

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from mcp.server.fastmcp import FastMCP
+import tools
+
+mcp = FastMCP('gici-lib mcp')
+mcp.add_tool(tools.list_directory, name='list_directory', description='List files and directories in the knowledge base root directory.')
+mcp.add_tool(tools.list_directory_recursive, name='list_directory_recursive', description='Recursively list all files and directories in a specified path within the knowledge base root directory.')
+mcp.add_tool(tools.read_file, name='read_file', description='Read the content of a specified file within the knowledge base root directory.')
+
 def main():
-    print("Hello from ai-mcp!")
+    mcp.run('stdio')
 
 
 if __name__ == "__main__":
